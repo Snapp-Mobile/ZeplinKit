7ca0caff78b8811403ed8670fdad263d89558d9a
Ilian Konchev <konchev@gmail.com>
2022-07-01 11:10:46 +0300

    Fix the package manifest

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/Snapp-Mobile/Fetcher", from: "0.0.1"),
         .package(url: "https://github.com/marmelroy/Zip", from: "2.0.0")
-    ]
+    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
