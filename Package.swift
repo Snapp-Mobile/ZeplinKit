4a9726a83ed813d9fe4300d54afa282b601db20e
Oleksii Kolomiiets <oleksii@snapp.team>
2026-01-12 09:22:14 +0100

    restructured folders

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version: 5.6
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -7,33 +6,31 @@ let package = Package(
     name: "ZeplinKit",
     platforms: [
         .iOS(.v13),
-        .macOS(.v11)
+        .macOS(.v11),
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ZeplinKit",
             targets: ["ZeplinKit"]
-        ),
+        )
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/Snapp-Mobile/Fetcher", from: "0.0.2"),
-        .package(url: "https://github.com/marmelroy/Zip", from: "2.0.0")
+        .package(url: "https://github.com/marmelroy/Zip", from: "2.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ZeplinKit",
             dependencies: ["Fetcher", "Zip"],
+            path: "Sources",
             resources: [
                 .process("Mocks")
             ]
         ),
         .testTarget(
             name: "ZeplinKitTests",
-            dependencies: ["ZeplinKit", "Fetcher"]
-        )
+            dependencies: ["ZeplinKit", "Fetcher"],
+            path: "Tests",
+        ),
     ]
 )
